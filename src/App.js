6417fdcd0c8b82a2678fa3b7383a70b5059eba9d
Ponmile Adebisi <ponmileadebisi@gmail.com>
2022-09-01 19:53:01 +0100

    css variable

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Contact from "./components/Contact";
+import "./App.css";
 
 function App() {
   return (
     <main>
       <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Contact />
     </main>
   );
 }
