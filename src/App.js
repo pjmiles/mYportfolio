8437389f71c4806d63b1cda4d77cab73a4c23227
Ponmile Adebisi <ponmileadebisi@gmail.com>
2022-09-01 19:43:38 +0100

    new look

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
-import { Routes, Route } from 'react-router-dom';
 import './App.css';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './components/Navbar';
-import { Header } from './components/Header';
-import About from './components/about/About';
-import Skills from './components/Skills';
 
 function App() {
   return (
-    <div>
+    <main>
       <Navbar />
-      <Routes>
-        <Route exact path='/' element={<Header />} />
-        <Route path='/about' element={<About />}/>
-        <Route path='/skills' element={<Skills />}/>
-      </Routes>
-    </div>
+    </main>
   );
 }
 
