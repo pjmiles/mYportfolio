0a7c61109752751c1e5cce3dc3f9338dc81934dc
Ponmile Adebisi <ponmileadebisi@gmail.com>
2022-09-06 18:34:39 +0100

    webkit added to project style

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,48 +1,61 @@
 import "../styles/Projects.css";
-
+import twitterProject from "../images/twitterclone.png";
+import blogProject from "../images/blogsite.png";
+import imageUpload from "../images/imageUpload.png";
+import urlShortner from "../images/urlshortner.png";
+import todoapp from "../images/todoapp.png";
 const data = [
   {
     id: 1,
-    picture: "twitter clone",
+    pics: twitterProject,
     title: "Twiter-Clone",
     github: "",
     demo: "https://react-twitter-clone-gamma.vercel.app",
   },
   {
     id: 2,
-    picture: "url-shortner",
-    title: "Url-Shortner",
-    github: "urlhere",
-    demo: "https://url-shortner-alpha-one.vercel.app",
-  },
-  {
-    id: 3,
-    picture: "blog without auth",
+    pics: blogProject,
     title: "Blog site",
     github: "urlhere",
     demo: "https://pjmiles-blog.vercel.app",
   },
   {
-    id: 4,
-    picture: "image search",
+    id: 3,
+    pics: imageUpload,
     title: "Image-Uploads",
     github: "urlhere",
     demo: "https://react-image-post.vercel.app",
+  },
+  {
+    id: 4,
+    pics: todoapp,
+    title: "Todo-App",
+    github: "urlhere",
+    demo: "https://react-image-post.vercel.app",
+  },
+  {
+    id: 5,
+    pics: urlShortner,
+    title: "Url-Shortner",
+    github: "urlhere",
+    demo: "https://react-todo-app-six-delta.vercel.app",
   },
 ];
 
 const Projects = () => {
   return (
     <section id="projects">
-      <h1>My Recent Work</h1>
+      <h1 className="project-header-text">My Recent Work</h1>
       <div className="projects-container">
-        {data.map(({ id, picture, title, github, demo }) => {
+        {data.map(({ id, pics, title, github, demo }) => {
           return (
             <article key={id} className="project-item">
-              <div>
-                <div>{picture}</div>
+              <div className="image-container hover">
+                <a href={demo} rel="noreferrer" target="_blank">
+                  <figure><img src={pics} alt="project" className="project-image" /></figure>
+                <span>{title}</span>
+                </a>
               </div>
-              <h2>{title}</h2>
               <div>
                 <a
                   href={github}
@@ -51,9 +64,6 @@ const Projects = () => {
                   className="btn"
                 >
                   github
-                </a>
-                <a href={demo} target="_blank" rel="noreferrer" className="btn">
-                  demo
                 </a>
               </div>
             </article>
