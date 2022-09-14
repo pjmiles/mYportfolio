711914c17f08a59f9315e6949afeeda7a1aec69e
Ponmile Adebisi <ponmileadebisi@gmail.com>
2022-09-14 08:57:14 +0100

    footer styled

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,8 @@
 import { FaGithub, FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa";
-import { HashLink as Link } from "react-router-hash-link";
+import { AiFillLinkedin } from "react-icons/ai";
+// import { HashLink as Link } from "react-router-hash-link";
+import { Link } from "react-router-dom";
+import "../styles/Footer.css";
 const links = [
   {
     id: 1,
@@ -24,16 +27,63 @@ const links = [
 ];
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+  const yearTxt = currentYear === 2022 ? "2022" : "2022 - " + currentYear;
   return (
     <footer className="footer-section">
-      Footer section
-      <div>
+      <div className="footer-icons">
+        <a
+          href="https://web.facebook.com/oluwaponmile.j.adebisi/"
+          target="_blank"
+          rel="noreferrer"
+          className="foot-icon"
+        >
+          <AiFillLinkedin />
+        </a>
+        <a
+          href="https://web.facebook.com/oluwaponmile.j.adebisi/"
+          target="_blank"
+          rel="noreferrer"
+          className="foot-icon"
+        >
+          <FaInstagram />
+        </a>
+        <a
+          href="https://github.com/pjmiles"
+          target="_blank"
+          rel="noreferrer"
+          className="foot-icon"
+        >
+          <FaGithub />
+        </a>
+        <a
+          href="https://github.com/pjmiles"
+          target="_blank"
+          rel="noreferrer"
+          className="foot-icon"
+        >
+          <FaTwitter />
+        </a>
+        <a
+          href="https://web.facebook.com/oluwaponmile.j.adebisi/"
+          target="_blank"
+          rel="noreferrer"
+          className="foot-icon"
+        >
+          <FaFacebookF />
+        </a>
+        <p className="footer-text">
+          Copyright &copy; {yearTxt} - Created by Ponmile Adebisi
+        </p>
+      </div>
+
+      {/* <div>
         {links.map(({ id, icon, href }) => {
           <ul key={id}>
             <Link to={href} className="foot-icon">{icon}</Link>
           </ul>;
         })}
-      </div>
+      </div> */}
     </footer>
   );
 };
