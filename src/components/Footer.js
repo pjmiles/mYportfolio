935956b3936f36ce6fd5427715ec9e75814e79b6
Ponmile Adebisi <ponmileadebisi@gmail.com>
2022-09-14 18:17:22 +0100

    fixing responsiveness for navbar and menu display

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,30 +1,6 @@
 import { FaGithub, FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa";
 import { AiFillLinkedin } from "react-icons/ai";
-// import { HashLink as Link } from "react-router-hash-link";
-import { Link } from "react-router-dom";
 import "../styles/Footer.css";
-const links = [
-  {
-    id: 1,
-    icon: <FaFacebookF />,
-    href: "https://web.facebook.com/oluwaponmile.j.adebisi/",
-  },
-  {
-    id: 2,
-    icon: <FaGithub />,
-    href: "https://github.com/pjmiles",
-  },
-  {
-    id: 3,
-    icon: <FaInstagram />,
-    href: "https://github.com/pjmiles",
-  },
-  {
-    id: 4,
-    icon: <FaTwitter />,
-    href: "https://github.com/pjmiles",
-  },
-];
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
@@ -33,7 +9,7 @@ const Footer = () => {
     <footer className="footer-section">
       <div className="footer-icons">
         <a
-          href="https://web.facebook.com/oluwaponmile.j.adebisi/"
+          href="https://www.linkedin.com/in/ponmileadebisi/"
           target="_blank"
           rel="noreferrer"
           className="foot-icon"
@@ -41,7 +17,7 @@ const Footer = () => {
           <AiFillLinkedin />
         </a>
         <a
-          href="https://web.facebook.com/oluwaponmile.j.adebisi/"
+          href="https://www.instagram.com/ponmilejames/"
           target="_blank"
           rel="noreferrer"
           className="foot-icon"
@@ -57,7 +33,7 @@ const Footer = () => {
           <FaGithub />
         </a>
         <a
-          href="https://github.com/pjmiles"
+          href="https://twitter.com/adebisiponmile"
           target="_blank"
           rel="noreferrer"
           className="foot-icon"
@@ -76,14 +52,6 @@ const Footer = () => {
           Copyright &copy; {yearTxt} - Created by Ponmile Adebisi
         </p>
       </div>
-
-      {/* <div>
-        {links.map(({ id, icon, href }) => {
-          <ul key={id}>
-            <Link to={href} className="foot-icon">{icon}</Link>
-          </ul>;
-        })}
-      </div> */}
     </footer>
   );
 };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,49 +5,53 @@ import { FaBars } from "react-icons/fa";
 import "../styles/Navbar.css";
 const Navbar = () => {
   const [activeNav, setActiveNav] = useState("#");
+  const [displayMenu, setDisplayMenu] = useState("")
+  const toggle = () => {
+
+  }
   return (
     <nav className="navbar ">
-      <div className="nav-section container">
-        <div className="brand-name">Ponmile Adebisi</div>
-        <FaBars className="hammburger" />
-        <ul className="nav-side">
-          <li className="nav-item">
-            <NavLink
-              to="#about"
-              onClick={() => setActiveNav("#about")}
-              className={activeNav === "#about" ? "active" : "nav-item"}
-            >
-              About
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink
-              to="#skills"
-              onClick={() => setActiveNav("#skills")}
-              className={activeNav === "#skills" ? "active" : "nav-item"}
-            >
-              Skills
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink
-              to="#projects"
-              onClick={() => setActiveNav("#projects")}
-              className={activeNav === "#projects" ? "active" : "nav-item"}
-            >
-              Projects
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink
-              to="#contact"
-              onClick={() => setActiveNav("#contact")}
-              className={activeNav === "#contact" ? "active" : "nav-item"}
-            >
-              Contact
-            </NavLink>
-          </li>
-        </ul>
+      <h1 className="brand-name">Ponmile Adebisi</h1>
+      <ul className="nav-side">
+        <li className="nav-item">
+          <NavLink
+            to="#about"
+            onClick={() => setActiveNav("#about")}
+            className={activeNav === "#about" ? "active" : "nav-item"}
+          >
+            About
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink
+            to="#skills"
+            onClick={() => setActiveNav("#skills")}
+            className={activeNav === "#skills" ? "active" : "nav-item"}
+          >
+            Skills
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink
+            to="#projects"
+            onClick={() => setActiveNav("#projects")}
+            className={activeNav === "#projects" ? "active" : "nav-item"}
+          >
+            Projects
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink
+            to="#contact"
+            onClick={() => setActiveNav("#contact")}
+            className={activeNav === "#contact" ? "active" : "nav-item"}
+          >
+            Contact
+          </NavLink>
+        </li>
+      </ul>
+      <div className="burger-menu">
+        <FaBars className="hammburger" onClick={toggle} />
       </div>
     </nav>
   );
