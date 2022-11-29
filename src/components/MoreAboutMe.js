87b90a85a86b0e344fb8578e5939efaaeac9a778
Ponmile Adebisi <ponmileadebisi@gmail.com>
2022-11-29 20:43:04 +0100

    MoreAbouteMe section text edit

--- src/components/MoreAboutMe.js
+++ src/components/MoreAboutMe.js
@@ -1,4 +1,4 @@
-import '../styles/MoreAboutMe.css'
+import "../styles/MoreAboutMe.css";
 
 const MoreAboutMe = () => {
   return (
@@ -7,23 +7,22 @@ const MoreAboutMe = () => {
       <p>
         In my career I started out as a Sales Executive where I gained
         experience in Sales, Marketing, Market Research and International
-        Business Development for 5years which I also gained Google Digital
-        Marketing Certification in my quest to stay upto date in this field.
+        Business Development for 5years. I pioneered new product launch across 3
+        regions in Nigeria and increased company's clients database.
       </p>
       <p>
         Out of my passion to explore more, I started out in Tech in 2020 during
         Covid period by learning Data Analytics where I gained Google Data
-        Analytics professional Certification after which I feel I could explore
-        more as a creative person, this led me to Software Engineering path. <br/> I
-        started self learning Software Engineering with FreeCodeCamp, Udemy and
-        Youtube. After months of self learning I got a new position as a
-        Software Engineer Intern with Robots Limited where I worked with Senior
-        Software Engineers and other teams members on projects.{" "}
+        Analytics Professional Certification after which I feel I could explore
+        more as a creative person, this led me to Software Engineering path.{" "}
+        <br /> I started self learning Software Engineering with FreeCodeCamp,
+        Udemy and Youtube. After months of self learning I got a new position as
+        a Software Engineer Intern with Robots Limited where I worked with
+        Senior Software Engineers and other teams members on projects.{" "}
       </p>
       <p>
         I am a curious person and love learning. In addition, when I am not
-        coding I produce music, I play the piano, drumset, love mixing sound and
-        also sound design.
+        coding I play the piano, love mixing sound and also sound design.
       </p>
     </section>
   );
