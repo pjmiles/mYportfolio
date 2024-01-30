46494853c94e8b3515460725dcb233f57c95a03c
pjmiles <ponmileadebisi@gmail.com>
2024-01-30 21:02:59 +0100

    more about adjusted

--- src/components/MoreAboutMe.js
+++ src/components/MoreAboutMe.js
@@ -10,14 +10,14 @@ const MoreAboutMe = () => {
         Business Development for 5years. I pioneered new product launch across 3
         regions in Nigeria and increased company's clients database.
       </p>
-      <p>
+      {/* <p>
         Out of my passion to explore more, I started self learning Software
         Engineering with FreeCodeCamp, Udemy and Youtube. After which I got a
         new position as a Software Engineer Intern with Robots Limited where I
         worked with Senior Software Engineers and crossfunctional teams members
         on projects. I currently work with Gugugeeks Royalty Limited as a Full
         Stack Developer.
-      </p>
+      </p> */}
       <p>
         I am a curious person and love learning. In addition, when I am not
         coding I play the piano, love mixing sound and also sound design.
