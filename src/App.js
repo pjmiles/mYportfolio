bc7a38f38430706b7c3fee333acc7d73d361d8a7
pjmiles <ponmileadebisi@gmail.com>
2024-01-30 21:02:18 +0100

    seperator added to app

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import Seperator from "./components/Seperator";
 import "./App.css";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
     <main>
       <Navbar />
       <About />
+      <Seperator />
       <MoreAboutMe />
       <Skills />
       <Projects />
