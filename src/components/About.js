d0232147ca994fa03d29f6c420bff923f6709414
pjmiles <ponmileadebisi@gmail.com>
2024-01-30 21:02:39 +0100

    about section adjusted

--- src/components/About.js
+++ src/components/About.js
@@ -4,17 +4,14 @@ import profileImage from "../assets/ponmile_james_adebisi.jpg";
 const About = () => {
   return (
     <section id="about" className="about-section">
-      <img alt="ponmile" src={profileImage} className="my-image" />
+      <div>
+         <img alt="ponmile" src={profileImage} className="my-image" />
+      </div>
 
       <div className="about-header">
-        <h1 className="top-text">About Me</h1>
         <p className="about-bio">
-          I'm <span>Ponmile Adebisi</span>, a Software Engineer who is creative
-          and innovative. I have fundamental knowledge of Software Design and
-          development, system architecture and infrastucture, API Design,
-          Databases including frontend and backend technologies. I am currently
-          keeping upto date with JavaScript, React, Typescript, Vue, NextJS,
-          Express and Python alongside other frontend and backend technologies.
+          Hey, I'm <span>Ponmile.</span>,
+        <p>A Software Developer. I provide custom solutions to Business needs.</p>
         </p>
       </div>
     </section>
